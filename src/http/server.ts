18b0c254e8e040a936bdf2eb74ae14e27fcc0d2c
Fabricio Martins <fabriciocsmartins@gmail.com>
2024-09-09 22:00:14 -0300

    [update] update server with first post endpoint goals

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,6 +4,20 @@ import z from 'zod'
 
 const app = fastify()
 
+app.post('/goals', async request => {
+  const createGoalSchema = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  })
+
+  const body = createGoalSchema.parse(request.body)
+
+  await createGoal({
+    title: body.title,
+    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+  })
+})
+
 app
   .listen({
     port: 3333,
